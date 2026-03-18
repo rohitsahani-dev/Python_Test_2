e4b73c6393ac1c3aee655f23e27c1ee74b99eee5
Rohit <rohit.sahani.dev@gmail.com>
2026-03-18 17:40:32 +0545

    adding extra feature to dots

--- T_2.py
+++ T_2.py
@@ -1,13 +1,21 @@
 ## --Adding color to all the point in the plot using color attribute
-
+## --Adding size to each dot in plot
+## --Adding transparency to each dot
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.array([4,43,45,66,78,84,65,34,24,73])
-y = np.array([2,3,45,6,7,8,65,4,4,7])
+y = np.array([72,34,45,66,72,50,65,4,40,17])
 
 #The number of values of the color should be equall to the array size of all 
-color = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown"])
+color = np.array(["red","green","blue","yellow","pink",
+                  "black","orange","purple","beige","brown"])
 
-plt.scatter(x,y,c=color)
+#Each dot can have its own size using s
+size = np.array([20,50,100,200,500,1000,60,90,10,300])
+
+#Each dot can be made transparent using alpha = value
+
+plt.scatter(x,y,c=color,s=size,alpha=0.9)
+
 plt.show()
