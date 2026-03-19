f8d279610e643d0e9da37fdb8177e098240f7cb4
Rohit <rohit.sahani.dev@gmail.com>
2026-03-19 15:14:29 +0545

    using function to take y values

---
+++ T_5.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def graph_plot(x):
+    result = 2*x+3
+    return result
+    
+
+
+xCord = []
+yCord = []
+num = int(input("Enter how many value of x you want to enter : "))
+for i in range(num+1):
+    x=int(input(f"Enter {i+1} value :"))
+    print(f"X{i+1} : {x}")
+    xCord.append(x)
+    y =graph_plot(x)
+    print(f"Y{i+1} : {y}")
+    yCord.append(y)
+    
+plt.plot(xCord,yCord,marker = 'o')
+
+plt.show()
