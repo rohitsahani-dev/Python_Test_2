008f414d0fd61ae8003d247fbbe13b45bc2dc1eb
Rohit <rohit.sahani.dev@gmail.com>
2026-03-29 22:36:37 +0545

    learning about pandas and map

---
+++ T_16.py
@@ -0,0 +1,11 @@
+import pandas
+
+df = pandas.read_csv("data1.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+print(df)
